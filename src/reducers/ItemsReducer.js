f07d3af03efd592eb559c3d686fb6b577f564142
A Machalica <a.machalica@selleo.com>
2017-06-27 00:26:07 +0200

    Fixes items reducer structure

--- src/reducers/ItemsReducer.js
+++ src/reducers/ItemsReducer.js
@@ -2,10 +2,6 @@ import { handleActions } from 'redux-actions';
 import * as types from '../actions/ActionTypes';
 
 export default handleActions({
-  [types.ADD_ITEM]: (state, action) => ({
-     items: [...state.items, action.payload],
-  }),
-  [types.RESET]: (state, action) => ({
-    items: [],
-  })
-}, {items: []});
+  [types.ADD_ITEM]: (state, action) => [action.payload, ...state],
+  [types.RESET]: (state, action) => [],
+}, []);
